7e7aa3e3e4add51b10575360279dfd8a22d73aae
JairRamosG <ramosgonzalezjair@gmail.com>
2025-09-05 17:21:38 -0600

    Agregar función para generación de dataframe con los resultados de las corridas de los modelos

--- Tarea1/main.py
+++ Tarea1/main.py
@@ -4,6 +4,7 @@ import os
 from generar_particiones import generar_particiones_estratificadas
 from modelos import entrenar_modelo, predecir_modelo
 from medidas_desempeño import calcula_medidas
+from generar_tabla import generar_tabla
 
 base_path = os.path.dirname(__file__)
 csv_path = os.path.join(base_path, 'datasets', 'crx_limpio.csv')
@@ -11,23 +12,19 @@ df = pd.read_csv(csv_path)
 
 particiones = generar_particiones_estratificadas(df, 100)
 
-modelos = ['1NN', '3NN', '5NN', 'Naive Bayes']
-medidas_1NN = []
+nombre_modelos = ['1NN', '3NN', '5NN', 'Naive Bayes']
+df_promedios = []
 
-for X_Train, Y_Train, X_Test, Y_Test in particiones:
-    
-    modelo = '1NN'
-    one_NN = entrenar_modelo(modelo, X_Train, Y_Train)
-    Y_pred_one_NN = predecir_modelo(one_NN, X_Test)
-    df_medidas = calcula_medidas(Y_Test, Y_pred_one_NN)
-    medidas_1NN.append(df_medidas)
+for nombre in nombre_modelos:
+    medidas = []
 
-medidas_1NN = pd.concat(medidas_1NN).reset_index(drop = True)
-medidas_1NN['Iteración'] = medidas_1NN.groupby('Medida').cumcount() + 1
-resultado_final = medidas_1NN.pivot(index = 'Iteración', columns = 'Medida', values = 'Valor').reset_index(drop = True)
+    for X_Train, Y_Train, X_Test, Y_Test in particiones:
+        modelo = entrenar_modelo(nombre, X_Train, Y_Train)
+        Y_pred = predecir_modelo(modelo, X_Test)
+        df_medidas = calcula_medidas(Y_Test, Y_pred)
+        medidas.append(df_medidas)
 
-promedios = resultado_final.mean().to_frame(name = f'Promedios {modelo}')
+    df_promedios.append(generar_tabla(medidas, nombre))
 
-print(promedios)
-
+print(pd.concat(df_promedios, axis = 1))
 
